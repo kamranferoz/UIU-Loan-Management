fc4b6715458dca5c3000a63935fe1bac635a8ca5
Mohammad Fuad Ahmed <fuad.robi@gmail.com>
2016-03-25 22:18:23 +0600

    new file added.

---
+++ application/views/landing/loanApplicationSuccessful.php
@@ -0,0 +1,11 @@
+
+<div class="col-lg-8 landingRightPanel">
+
+    <div class="panel-body">
+        <div class="alert alert-info">
+            <p style="font-size: 20px">Your Form is submitted successfully.UIU authority will verify your information.
+                You can view your loan status by your user ID.</p>
+
+        </div>
+    </div>
+</div>
